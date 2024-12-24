b165c537b77c0183167738e2d0b4caae63e07bec
yeabtsega45 <yabtsega2022@gmail.com>
2024-12-24 18:38:31 +0300

    style signin and signup pages

--- src/routes/homepage/Homepage.jsx
+++ src/routes/homepage/Homepage.jsx
@@ -10,7 +10,7 @@ const Homepage = () => {
     <div className="homepage">
       <img src="/orbital.png" alt="" className="orbital" />
       <div className="left">
-        <h1>LAMA AI</h1>
+        <h1>YABU AI</h1>
         <h2>Supercharge your creativity and productivity</h2>
         <h3>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat sint
